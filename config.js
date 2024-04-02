48cd01aed2596e25d19f4256cb2cee042998741e
garenteeh <bmg.laurent@live.fr>
2024-04-02 15:31:31 +0200

    fix config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     accessKeyId: 'MVO07V0UXM1YHYZ2LTEK', // TODO
     secretAccessKey: 'A0FLyTzWOUQaRde4N2LY6hhj2sHEsF6BT6GNnzDd', // TODO
     endpoint: 's3.eu-west-2.wasabisys.com', // TODO
-    bucket: ' esgi-cloud-exam-bucket-32430', // TODO
+    bucket: 'esgi-cloud-exam-bucket-32430', // TODO
   },
   suffix: {
     small: '_small.jpg',
