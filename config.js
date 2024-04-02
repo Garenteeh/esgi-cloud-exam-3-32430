1a604e5667337ab786943acbbfce8b7c5cecac01
garenteeh <bmg.laurent@live.fr>
2024-04-02 15:21:28 +0200

    initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'MVO07V0UXM1YHYZ2LTEK', // TODO
+    secretAccessKey: 'A0FLyTzWOUQaRde4N2LY6hhj2sHEsF6BT6GNnzDd', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: ' esgi-cloud-exam-bucket-32430', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
